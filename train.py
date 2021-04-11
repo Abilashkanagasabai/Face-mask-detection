3dd68142415fd81042f10bbf071fab9118862bf7
Abilash K <47034540+Abilashkanagasabai@users.noreply.github.com>
2021-04-11 13:13:34 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+import cv2,os
+import numpy as np
+
+
+pf = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(pf[0],True)
+
+# re-size all the images to this
+IMAGE_SIZE = [224, 224]
+
+# train_path = '/media/tarun/New Volume/Face Mask detection/dataset/train'
+# valid_path = '/media/tarun/New Volume/Face Mask detection/dataset/test'
+
+
+
+
+
+data_path='/media/tarun/New Volume/Face Mask detection/Deep-Learning-Face-Recognition-master/dataset'
+categories=os.listdir(data_path)
+labels=[i for i in range(len(categories))]
+
+label_dict=dict(zip(categories,labels)) #empty dictionary
+
+print(label_dict)
+print(categories)
+print(labels)
+
+
+img_size=224
+data=[]
+target=[]
+
+
+for category in categories:
+    folder_path=os.path.join(data_path,category)
+    img_names=os.listdir(folder_path)
+        
+    for img_name in img_names:
+        img_path=os.path.join(folder_path,img_name)
+        img=cv2.imread(img_path)
+
+        try:
+            gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)           
+            #Coverting the image into gray scale
+            resized=cv2.resize(gray,(img_size,img_size))
+            #resizing the gray scale into 50x50, since we need a fixed common size for all the images in the dataset
+            data.append(resized)
+            target.append(label_dict[category])
+            #appending the image and the label(categorized) into the list (dataset)
+
+        except Exception as e:
+            print('Exception:',e)
+            #if any exception rasied, the exception will be printed here. And pass to the next image
+# add preprocessing layer to the front of VGG
+
+
+
+data=np.array(data)/255.0
+data=np.reshape(data,(data.shape[0],img_size,img_size,1))
+target=np.array(target)
+
+from keras.utils import np_utils
+
+new_target=np_utils.to_categorical(target)
+
+vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# don't train existing weights
+for layer in vgg.layers:
+  layer.trainable = False
+  
+
+  
+  # useful for getting number of classes
+folders = glob('/media/tarun/New Volume/Face Mask detection/dataset/train*')
+  
+
+# our layers - you can add more if you want
+x = Flatten()(vgg.output)
+# x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=vgg.input, outputs=prediction)
+
+# view the structure of the model
+model.summary()
+
+# tell the model what cost and optimization method to use
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+
+
+# fit the model
+r = model.fit(
+  data,
+  new_target,
+  epochs=5
+)
+# loss
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+
+# accuracies
+plt.plot(r.history['acc'], label='train acc')
+plt.plot(r.history['val_acc'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
+
+
+from tensorflow.keras.models import load_model
+
+model.save('model_detector.model')
